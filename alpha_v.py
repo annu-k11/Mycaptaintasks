275bec675f5e1460fb994679492a017ac44a8383
annu-k11 <85413092+annu-k11@users.noreply.github.com>
2021-06-06 22:50:36 +0530

    Delete alpha_v.py

--- alpha_v.py
+++
@@ -1,7 +0,0 @@
-letter = lambda str :dict([(i,str.count(i)) for i in str])
-[print( k, " = ", v ) for k,v in dict(sorted(letter(input("Enter a word :").lower()).items(), key = lambda kv : kv[1], reverse = True)).items()]
-
-
-
-
-#[print( k, " = ", v ) for k,v in dict(sorted(dict([(i,str.count(i)) for i in input("Enter :").lower()]).items(), key = lambda kv : kv[1], reverse = True)).items()]
